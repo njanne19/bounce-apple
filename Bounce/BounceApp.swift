e4714a79ecb90a2b2b1f62556669f92fe11f024e
njanne19 <njanne@umich.edu>
2025-02-18 20:56:49 -0500

    Initial Commit

---
+++ Bounce/BounceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BounceApp.swift
+//  Bounce
+//
+//  Created by Nick on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct BounceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BounceTests/BounceTests.swift
@@ -0,0 +1,17 @@
+//
+//  BounceTests.swift
+//  BounceTests
+//
+//  Created by Nick on 2/18/25.
+//
+
+import Testing
+@testable import Bounce
+
+struct BounceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
